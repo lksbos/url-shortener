8aa039aad54dd238b133b2843af687bc1ad49ce0
Lucas Silva <lksbos@gmail.com>
2020-04-30 09:28:03 -0700

    First commit.

---
+++ src/test/java/com/edu/urlshortener/UrlshortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.urlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlshortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
